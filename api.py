515d9b0e0c0bada849da358cb749ba6ed5c84e3b
cmasso6 <clement.massonnaud@gmail.com>
2021-04-20 16:02:48 +0200

    update api.py

--- api.py
+++ api.py
@@ -2,6 +2,7 @@ import cbpro
 import json
 import subprocess
 import os
+import sys
 from datetime import datetime
 
 public_client = cbpro.PublicClient()
@@ -9,7 +10,7 @@ prices_file = f'/home/{os.getlogin()}/.tmp/last-prices.json'
 products = ['BTC', 'ADA', 'ETH']
 threshold_pump = 5
 threshold_dump = -5
-delayNotif = 3600 # seconds
+delayNotif = int(sys.argv[1]) # seconds
 
 # Read last prices
 with open(prices_file) as f:
@@ -52,7 +53,7 @@ for p in products:
         notif = f'{p}: {diffs[p][0]} %'
         subprocess.call(["/home/cmasso6/src/notif-crypto", "dump", notif])
 
-if tdelta.seconds > delayNotif:    
+if tdelta.seconds > delayNotif:
     notif = f'BTC: {prices["BTC"][0]} / {prices["BTC"][1]}\nETH: {prices["ETH"][0]} / {prices["ETH"][1]}\nADA: {prices["ADA"][0]} / {prices["ADA"][1]}'
     subprocess.call(["/home/cmasso6/src/notif-crypto", "notif", notif])
 
